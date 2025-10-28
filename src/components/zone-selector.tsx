887d6dda4822e918b7f8f6f49eac3ff541ada72b
ZerglingGo <zerglinggo@zerglinggo.net>
2025-10-28 21:42:19 +0900

    style: mobile responsive

--- src/components/zone-selector.tsx
+++ src/components/zone-selector.tsx
@@ -9,7 +9,7 @@ export default function ZoneSelector({ date, setZone }: { date: Date; setZone: (
   const { data } = useSWR<Zone[]>(`/api/zones?date=${dateString}`);
 
   return (
-    <div className="mt-8 grid grid-cols-3 gap-4">
+    <div className="mt-8 grid grid-cols-2 gap-4 lg:grid-cols-3">
       {data?.map((zone) => (
         <button key={`zone-${zone.id}`} type="button" className="cursor-pointer rounded-lg p-2 text-left transition hover:bg-neutral-100" onClick={() => setZone(zone)}>
           <div className="relative aspect-square w-full overflow-hidden rounded-lg">
@@ -17,8 +17,8 @@ export default function ZoneSelector({ date, setZone }: { date: Date; setZone: (
           </div>
 
           <div className="mt-2">
-            <h2 className="mb-1 font-extrabold text-2xl">{zone.name}</h2>
-            <span className="font-bold text-lg text-red-500">{zone.price.toLocaleString("ko-KR")}원</span>
+            <h2 className="font-extrabold text-lg xl:mb-1 xl:text-2xl">{zone.name}</h2>
+            <span className="font-bold text-red-500 text-sm xl:text-lg">{zone.price.toLocaleString("ko-KR")}원</span>
           </div>
         </button>
       ))}
